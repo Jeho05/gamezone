f8b60301b70820c3bfe18b153abd5dd558400841
Jehojada <saccajeho@gmail.com>
2025-10-24 20:43:28 +0200

    Deploy to GitHub Pages

---
+++ assets/createLucideIcon-CyDeinbq.js
@@ -0,0 +1,11 @@
+import{r as i}from"./react-vendor-Ct9RIpkg.js";if(typeof window<"u"){const o={NEXT_PUBLIC_API_BASE:"https://votre-domaine.infinityfreeapp.com/api",NEXT_PUBLIC_KKIAPAY_PUBLIC_KEY:"072b361d25546db0aee3d69bf07b15331c51e39f",NEXT_PUBLIC_KKIAPAY_SANDBOX:"0"};globalThis.process??={};const n=globalThis.process.env??{};globalThis.process.env=new Proxy(Object.assign({},o,n),{get(e,r){return r in e?e[r]:void 0},has(){return!0}})}/**
+ * @license lucide-react v0.358.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var _={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};if(typeof window<"u"){const o={NEXT_PUBLIC_API_BASE:"https://votre-domaine.infinityfreeapp.com/api",NEXT_PUBLIC_KKIAPAY_PUBLIC_KEY:"072b361d25546db0aee3d69bf07b15331c51e39f",NEXT_PUBLIC_KKIAPAY_SANDBOX:"0"};globalThis.process??={};const n=globalThis.process.env??{};globalThis.process.env=new Proxy(Object.assign({},o,n),{get(e,r){return r in e?e[r]:void 0},has(){return!0}})}/**
+ * @license lucide-react v0.358.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=o=>o.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();if(typeof window<"u"){const o={NEXT_PUBLIC_API_BASE:"https://votre-domaine.infinityfreeapp.com/api",NEXT_PUBLIC_KKIAPAY_PUBLIC_KEY:"072b361d25546db0aee3d69bf07b15331c51e39f",NEXT_PUBLIC_KKIAPAY_SANDBOX:"0"};globalThis.process??={};const n=globalThis.process.env??{};globalThis.process.env=new Proxy(Object.assign({},o,n),{get(e,r){return r in e?e[r]:void 0},has(){return!0}})}const P=(o,n)=>{const e=i.forwardRef(({color:r="currentColor",size:t=24,strokeWidth:a=2,absoluteStrokeWidth:c,className:d="",children:s,...b},p)=>i.createElement("svg",{ref:p,..._,width:t,height:t,stroke:r,strokeWidth:c?Number(a)*24/Number(t):a,className:["lucide",`lucide-${f(o)}`,d].join(" "),...b},[...n.map(([l,u])=>i.createElement(l,u)),...Array.isArray(s)?s:[s]]));return e.displayName=`${o}`,e};export{P as c};
